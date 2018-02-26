cd62b6d694339b8416f81910657b34275e02d99c
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-02-26 16:04:13 +0000

    improved ui
    added back link and more writing

--- public/admin/subjects/show.php
+++ public/admin/subjects/show.php
@@ -1,7 +1,17 @@
 <?php require_once('../../../private/init.php'); ?>
 <?php
     // $id = isset($_GET['id']) ? $_GET['id'] : '1'; // PHP < 7.0
-
     $id = $_GET['id'] ?? '1'; // PHP > 7.0
-    echo h($id);
 ?>
+
+<?php $page_title = 'Show Subject'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+    <a class="back-link" href="<?php echo url_for('/admin/subjects/index.php'); ?>">&laquo; Back to List</a>
+    <div class="subject show">
+        Section ID: <?php echo h($id); ?>
+    </div>
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
