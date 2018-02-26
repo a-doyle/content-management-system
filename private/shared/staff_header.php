c6f2d5eb4e8fcbc460d225cc858aeaa7fc55196e
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-02-26 12:14:50 +0000

    added doc root to fix redirect issues

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -1,7 +1,11 @@
+<?php
+    if(!isset($page_title)) { $page_title = 'Staff Area'; }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>GBI: Globe Bank International</title>
+    <title>GBI: Globe Bank International - <?php echo $page_title; ?></title>
     <meta charset="utf-8">
     <link rel="stylesheet" media="all" href="../css/app.css">
 </head>
@@ -14,6 +18,6 @@
 
     <nav>
         <ul>
-            <li><a href="index.php">Menu</a></li>
+            <li><a href="<?php echo WWW_ROOT . '/admin/index.php'; ?>">Menu</a></li>
         </ul>
     </nav>
