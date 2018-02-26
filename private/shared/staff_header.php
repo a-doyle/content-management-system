eedaedda90ec3d7fa73f0f6040118171e16f7919
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-02-26 11:28:44 +0000

    chopped and changed html blocks
    As this was all reusable code, it made sense to have them in separate files to be referenced by the PHP.

---
+++ private/shared/staff_header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>GBI: Globe Bank International</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="../css/app.css">
+</head>
+
+<body>
+
+    <header>
+        <h1>GBI Staff Area</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="index.php">Menu</a></li>
+        </ul>
+    </nav>
+
+</body>
+</html>
