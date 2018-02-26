94b02d6345e41e9aa26172414c15f83024729b69
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-02-26 11:42:58 +0000

    Add files via upload

---
+++ public/admin/subjects/index.php
@@ -0,0 +1,10 @@
+<?php require_once('../../../private/init.php'); ?>
+
+<?php $page_title = 'Subjects'; ?>
+<?php include('../../../private/shared/staff_header.php'); ?>
+
+<div id="content">
+
+</div>
+
+<?php include('../../../private/shared/staff_footer.php'); ?>
