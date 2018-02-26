5575f8aa8947b58575def08a1fdc4d1ff534c345
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-02-26 11:21:20 +0000

    connected php

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<?php require_once('init.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>GBI: Globe Bank International</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="../css/app.css">
+</head>
+
+<body>
+
+    <header>
+        <h1>GBI Staff Area</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="index.php">Menu</a></li>
+        </ul>
+    </nav>
+
+    <div id="content">
+
+    </div>
+
+    <footer>
+        &copy; <?php echo date('Y'); ?> Globe Bank
+    </footer>
+
+</body>
+</html>
