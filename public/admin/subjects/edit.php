e9d7f6ec461bc579c1e30083b8997a4a8a717f4c
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-02-26 16:43:18 +0000

    implemented single form processing

--- public/admin/subjects/edit.php
+++ public/admin/subjects/edit.php
@@ -2,15 +2,25 @@
 
 require_once('../../../private/init.php');
 
-$test = $_GET['test'] ?? '';
-
-if($test == '404') {
-    error_404();
-} elseif($test == '500') {
-    error_500();
-} elseif($test == 'redirect') {
-    redirect_to(url_for('/admin/subjects/index.php'));
+if(!isset($_GET['id'])) {
+  redirect_to(url_for('/admin/subjects/index.php'));
 }
+$id = $_GET['id'];
+
+if(is_post_request()) {
+  // Handle form values sent by new.php
+  $menu_name = $_POST['menu_name'] ?? '';
+  $position = $_POST['position'] ?? '';
+  $visible = $_POST['visible'] ?? '';
+
+  echo "Form parameters<br>";
+  echo "Menu name: " . $menu_name . "<br>";
+  echo "Position: " . $position . "<br>";
+  echo "Visible: " . $visible . "<br>";
+} else {
+  //redirect_to(url_for('/admin/subjects/new.php'));
+}
+
 ?>
 
 <?php $page_title = 'Edit Subject'; ?>
@@ -23,7 +33,7 @@ if($test == '404') {
   <div class="subject edit">
     <h1>Edit Subject</h1>
 
-    <form action="" method="post">
+    <form action="<?php echo url_for('/admin/subjects/edit.php?id=' . h(u($id))); ?>" method="post">
       <dl>
         <dt>Menu Name</dt>
         <dd><input type="text" name="menu_name" value="" /></dd>
