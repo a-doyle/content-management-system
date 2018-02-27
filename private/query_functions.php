f7657672a6900d2e5b10639072a6d0542dfcba81
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-02-27 15:22:26 +0000

    function to create a record

--- private/query_functions.php
+++ private/query_functions.php
@@ -21,6 +21,26 @@
         return $subject; // returns an assoc. array
     }
 
+    function insert_subject($menu_name, $position, $visible) {
+        global $db;
+        $sql = "INSERT INTO subjects ";
+        $sql .= "(menu_name, position, visible) ";
+        $sql .= "VALUES (";
+        $sql .= "'" . $menu_name . "',";
+        $sql .= "'" . $position . "',";
+        $sql .= "'" . $visible . "'";
+        $sql .= ")";
+        $result = mysqli_query($db, $sql);
+        // for INSERT statements, $result is true/false
+        if($result) {
+            return true;
+        } else {
+            echo mysqli_error($db);
+            db_disconnect($db);
+            exit;
+        }
+    }
+
     function find_all_pages() {
         global $db;
         $sql = "SELECT * FROM pages ";
