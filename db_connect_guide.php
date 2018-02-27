1bef31481e58fcee929143ec09eeec97de1108b8
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-02-27 12:08:01 +0000

    updated connection guide

--- db_connect_guide.php
+++ db_connect_guide.php
@@ -4,18 +4,21 @@
 
 // Credentials
 $dbhost = 'localhost';
-$dbuser = '#';
-$dbpass = '#';
+$dbuser = 'doyyyler';
+$dbpass = '@land0Yl3';
 $dbname = 'globe_bank';
 
 // 1. Create a database connection
 $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
 // 2. Perform database query
+$query = "SELECT * FROM subjects";
+$result_set = mysqli_query($connection, $query);
 
 // 3. Use returned data (if any)
 
 // 4. Release returned data
+mysqli_free_result($result_set);
 
 // 5. Close database connection
 mysqli_close($connection);
