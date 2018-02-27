0718a9ddbcd52dd9f73817315cd46ec7c522e911
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-02-27 15:02:12 +0000

    added function to return data for single subject

--- private/query_functions.php
+++ private/query_functions.php
@@ -10,6 +10,17 @@
         return $result;
     }
 
+    function find_subject_by_id($id) {
+        global $db;
+        $sql = "SELECT * FROM subjects ";
+        $sql .= "WHERE id='" . $id . "'";
+        $result = mysqli_query($db, $sql);
+        confirm_result_set($result);
+        $subject = mysqli_fetch_assoc($result);
+        mysqli_free_result($result);
+        return $subject; // returns an assoc. array
+    }
+
     function find_all_pages() {
         global $db;
         $sql = "SELECT * FROM pages ";
