e07d8f6fbe34a31831a502150f8beffb77e397e5
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-02-27 11:44:39 +0000

    a guide to connecting a database with PHP

---
+++ db_connect_guide.php
@@ -0,0 +1,23 @@
+<? 
+
+// This guide demonstrates the five fundamental steps of database interaction with PHP
+
+// Credentials
+$dbhost = 'localhost';
+$dbuser = '#';
+$dbpass = '#';
+$dbname = 'globe_bank';
+
+// 1. Create a database connection
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+// 2. Perform database query
+
+// 3. Use returned data (if any)
+
+// 4. Release returned data
+
+// 5. Close database connection
+mysqli_close($connection);
+
+?>
