811211e0de658e10e1158b094f75a4fdc6c3afd7
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-03-05 15:31:23 +0000

    added UI for public access

---
+++ private/shared/public_footer.php
@@ -0,0 +1,10 @@
+    <footer>
+        <p>Copyright <?php echo date('Y'); ?>, Globe Bank International</p>
+    </footer>
+
+    <p>This is a fictitious company, website designed by Alan Doyle, under the direction of <a href="http://linkedin.com">LinkedIn Corporation, or its affiliates</a>, solely for the creation and devlopment of his skillset. Any resemblence to real products or services is purely coincidental. Information provided about the products or services is also fictitious and should not be construed as representative of actual products or services on the market in similar product or service category.</p>
+
+</body>
+</html>
+
+<?php db_disconnect($db); ?>
---
+++ private/shared/public_header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <title>Globe Bank <?php if(isset($page_title)) { echo '- ' . h($page_title); } ?></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="<?php echo url_for('css/public_app.css'); ?>">
+</head>
+
+<body>
+    <header>
+        <h1><a href="<?php echo url_for('/index.php'); ?>"><img src="<?php echo url_for('img/gbi_logo.png'); ?>" width="298" height="71" alt=""></a></h1>
+    </header>
