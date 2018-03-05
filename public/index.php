fe8601abd234a631e23624770371fe7eb5b7854d
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-03-05 16:37:33 +0000

    added functionality to allow nav to work between pages

--- public/index.php
+++ public/index.php
@@ -1,11 +1,36 @@
 <?php require_once('../private/init.php'); ?>
 
+<?php
+    if(isset($_GET['id'])) {
+        $page_id = $_GET['id'];
+        $page = find_page_by_id($page_id);
+        if(!$page) {
+            redirect_to(url_for('/index.php'));
+        } 
+    } else {
+        // nothing selected; show the homepage
+    }
+?>
+
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
 <div id="main">
     <?php include(SHARED_PATH . '/public_nav.php'); ?>
     <div id="page">
-
+    <?php
+        if(isset($page)) {
+            // show the page from the database
+            // TODO add html escaping back in
+            echo $page['content'];
+        } else {
+            // Show the homepage
+            // Homepage content coukd:
+            // * be static content (here or in a shared file)
+            // * show the first page from the nav
+            // * be in the database but add code to hide in the nav
+            include(SHARED_PATH . '/static_homepage.php'); 
+        }  
+    ?>
     </div>
 </div>
 
