df212f6f5b11fdc973cd9d1e649a14e726e300ab
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-03-05 10:19:26 +0000

    added redirect links to each functional button

--- public/admin/subjects/index.php
+++ public/admin/subjects/index.php
@@ -34,7 +34,7 @@
     	    <td><?php echo h($subject['menu_name']); ?></td>
           <td><a class="action" href="<?php echo url_for('/admin/subjects/show.php?id=' . h(u($subject['id']))); ?>">View</a></td>
           <td><a class="action" href="<?php echo url_for('/admin/subjects/edit.php?id=' . h(u($subject['id']))); ?>">Edit</a></td>
-          <td><a class="action" href="">Delete</a></td>
+          <td><a class="action" href="<?php echo url_for('/admin/subjects/delete.php?id=' . h(u($subject['id']))); ?>">Delete</a></td>
     	  </tr>
       <?php } ?>
 
