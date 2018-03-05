d5010937c15fe9a1cf086dd7c86ddf4d17ac0f51
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-03-05 12:30:37 +0000

    updated to include error validation

--- public/admin/subjects/edit.php
+++ public/admin/subjects/edit.php
@@ -16,10 +16,18 @@
     $subject['visible'] = $_POST['visible'] ?? '';
 
     $result = update_subject($subject);
-    redirect_to(url_for('/admin/subjects/show.php?id=' . $id));
-
+    if($result === true) {
+      redirect_to(url_for('/admin/subjects/show.php?id=' . $id));
+    } else {
+      $errors = $result;
+      var_dump($errors);
+    }
+  
   } else {
     $subject = find_subject_by_id($id);
+    $subject_set = find_all_subjects();
+    $subject_count = mysqli_num_rows($subject_set);
+    mysqli_free_result($subject_set);
   }
 
 ?>
@@ -43,7 +51,15 @@
         <dt>Position</dt>
         <dd>
           <select name="position">
-            <option value="1"<?php if($subject['position'] == "1") { echo " selected"; } ?>>1</option>
+            <?php
+              for($i=1; $i <= $subject_count; $i++) {
+                echo "<option value=\"{$i}\"";
+                if($subject['position'] == $i) { 
+                  echo " selected"; 
+                }
+                echo ">{$i}</option>";
+              }
+            ?>
           </select>
         </dd>
       </dl>
