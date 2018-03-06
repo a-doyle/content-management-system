e74b3b318263d97de0e560ba2e0cf2e493a2c338
Alan Doyle <36408396+a-doyle@users.noreply.github.com>
2018-03-06 15:40:24 +0000

    finalized authentication functionality

--- public/admin/logout.php
+++ public/admin/logout.php
@@ -1,9 +1,6 @@
 <?php
     require_once('../../private/init.php');
 
-    unset($_SESSION['username']);
-    // or you could use
-    // $_SESSION['username'] = NULL;
-
+    log_out_admin();
     redirect_to(url_for('/admin/login.php'));
 ?>
